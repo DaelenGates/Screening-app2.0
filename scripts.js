aa8a12a9a56b3709b51ac676441b913485566ffa
DaelenGates <115597283+DaelenGates@users.noreply.github.com>
2023-08-04 11:23:38 -0700

    Updated to Use mapbox API

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
+// AIzaSyA9c1IbHOE57PaRtxU_N4Rdit8Wad3Zma0 KEY
 // js for map 
 var map = L.map('map').setView([47.2452,-122.4582],12);
+// This ist just testing map2 which is to test the google maps api
+// var map2 = L.map('map2').setView([47.2452,-122.4582],12);
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     accessToken: 'pk.eyJ1IjoiZGFlbGVuZyIsImEiOiJjbGE4MnNpbjQwMHgxM29vMG1xNXA0YjR3In0.1m-yZapuOVRg2zWL8fimbw',
+// }).addTo(map2);
 
 var latlng = L.latLng(0,0);
 var marker = L.marker(latlng,{
@@ -21,6 +30,16 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_
 var tracts = L.geoJson(tract,{
   interactive: false,
   PointToLayer: function(feature, latlng){
+  }
+}).addTo(map);
+
+var tracts = L.geoJSON(tract, {
+  PointToLayer: function(feature, latlng){
+  },
+  onEachFeature: function (feature, layer) {
+    layer.on('click', function () {
+      console.log(feature.properties.TRACTCE);
+    });
   }
 }).addTo(map);
 
@@ -80,10 +99,6 @@ var tracts = L.geoJson(tract,{
 
 // This is where testing starts 
 
-// import * as GeoSearch from 'leaflet-geosearch@3.0.0';
-
-
-// L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // KEEP THIS MARKER CODE TO MANUALLY TEST IF MARKER IS INSIDE OF CENUS TRACTS FOR LATER 
 var latlng = L.latLng(47.2452,-122.4582);
@@ -118,7 +133,7 @@ map.on('click', function (e) {
   }
   // This iterates through the applicable census tracts and asks IF pt is inside of any of them one at at time 
   var turfPolygons = [];
-  var count = (0);
+  var count = (0); // this count is to count the number of census tracts, it maybe can be removed later
   var bool3 = (false);
   var boolf = (true);
   tract.features.forEach(function(feature) {
@@ -198,15 +213,140 @@ function createTextBoxControl(content) {
 // }).addTo(map);
 
 // Anouther try 
+// This is whats being recomended on Stack Overflow 
+// let googleGeoCoderProvider = L.Control.Geocoder.google({
+//   apiKey: 'AIzaSyA9c1IbHOE57PaRtxU_N4Rdit8Wad3Zma0',
+// }).on('markgeocode', function (event) {
+//   var location = event.geocode.center;
+
+//   // Remove the previous marker if it exists
+//   if (marker) {
+//       map.removeLayer(marker);
+//   }
+//   // Add a new marker at the selected location
+//   marker = L.marker(location).addTo(map);
+//   map.setView(location, 13);
+//   // Just testing getting lat lon points from leaflet-control-geocoder 
+//   // This is where code is reused to test if the geosearched locations is within one of the census tracts 
+//   var loc = marker.getLatLng();
+//   console.log("lat: ", loc.lat);
+//   console.log("long: ", loc.lng);
+//   var pt = turf.point([marker.getLatLng().lng, marker.getLatLng().lat]);
+//   console.log(pt);
+//   // This is supposed to iterate though each of the census tracts if bool3 comes out as true it logs, if not than it remains false 
+//   var turfPolygons = [];
+//   var count = (0);
+//   var bool3 = (false);
+//   var boolf = (true);
+//   tract.features.forEach(function(feature) {
+//     var polygon = turf.polygon(feature.geometry.coordinates);
+//     turfPolygons = (polygon);
+//     bool3 = turf.booleanPointInPolygon(pt, turfPolygons);
+//     count = (count + 1);
+//     if (bool3) {
+//       boolf = (false);
+//       createTextBoxControl("This address IS eligible");
+//     }
+//   });
+//   if (boolf) {
+//     createTextBoxControl("This address IS NOT eligible");
+//   }
+// }).addTo(map);
+
+// Try 2 
+// let googleGeoCoderProvider = L.Control.Geocoder.google({
+//   apiKey: 'AIzaSyAkYLkNcozn6vdqps5Pc0-ZNfIcawiavVg'
+// });
+// L.Control.geocoder({
+//   placeholder: 'Search for a location...',
+//   collapsed: false,
+//   geocoder: googleGeoCoderProvider,
+// }).addTo(map);
+
+// Try 3 
+// var geocoder = L.Control.geocoder({
+//   defaultMarkGeocode: false,
+//   collapsed: false,
+//   placeholder: 'Search for a location...',
+//   // geocoder: L.Control.Geocoder.google('AIzaSyA9c1IbHOE57PaRtxU_N4Rdit8Wad3Zma0')
+//   // geocoder: function(query, callback) {
+//   //   var geocoder = new google.maps.Geocoder();
+//   //   geocoder.geocode({ address: query }, function(results, status) {
+//   //     if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+//   //       var location = results[0].geometry.location;
+//   //       callback(location, results[0].formatted_address);
+//   //     } else {
+//   //       callback(null);
+//   //     }
+//   //   });
+//   // }
+//   // geocoder: L.Control.Geocoder.nominatim()
+//   // COME BACK TO THIS ONCE I HAVE A GOOGLE API KEY 
+//   // AIzaSyA9c1IbHOE57PaRtxU_N4Rdit8Wad3Zma0 KEY
+//   geocoder: L.Control.Geocoder.google({
+//       apiKey: 'AIzaSyA9c1IbHOE57PaRtxU_N4Rdit8Wad3Zma0' // Replace with your Google API key
+//   })
+// }).on('markgeocode', function (event) {
+//   var location = event.geocode.center;
+
+//   // Remove the previous marker if it exists
+//   if (marker) {
+//       map.removeLayer(marker);
+//   }
+//   // Add a new marker at the selected location
+//   marker = L.marker(location).addTo(map);
+//   map.setView(location, 13);
+//   // Just testing getting lat lon points from leaflet-control-geocoder 
+//   // This is where code is reused to test if the geosearched locations is within one of the census tracts 
+//   var loc = marker.getLatLng();
+//   console.log("lat: ", loc.lat);
+//   console.log("long: ", loc.lng);
+//   var pt = turf.point([marker.getLatLng().lng, marker.getLatLng().lat]);
+//   console.log(pt);
+//   // This is supposed to iterate though each of the census tracts if bool3 comes out as true it logs, if not than it remains false 
+//   var turfPolygons = [];
+//   var count = (0);
+//   var bool3 = (false);
+//   var boolf = (true);
+//   tract.features.forEach(function(feature) {
+//     var polygon = turf.polygon(feature.geometry.coordinates);
+//     turfPolygons = (polygon);
+//     bool3 = turf.booleanPointInPolygon(pt, turfPolygons);
+//     count = (count + 1);
+//     if (bool3) {
+//       boolf = (false);
+//       createTextBoxControl("This address IS eligible");
+//     }
+//   });
+//   if (boolf) {
+//     createTextBoxControl("This address IS NOT eligible");
+//   }
+// }).addTo(map);
+
+// Try 4 
+// let googleGeoCoderProvider = L.Control.Geocoder.google('AIzaSyAkYLkNcozn6vdqps5Pc0-ZNfIcawiavVg');
+
+// L.Control.geocoder({
+//   placeholder: 'Search for a location...',
+//   collapsed: false,
+//     geocoder: googleGeoCoderProvider,
+// }).addTo(map);
+
+// ORIGINAL works 
 var geocoder = L.Control.geocoder({
   defaultMarkGeocode: false,
   collapsed: false,
   placeholder: 'Search for a location...',
-  geocoder: L.Control.Geocoder.nominatim()
+  // geocoder: L.Control.Geocoder.nominatim()
   // COME BACK TO THIS ONCE I HAVE A GOOGLE API KEY 
+  // AIzaSyA9c1IbHOE57PaRtxU_N4Rdit8Wad3Zma0 KEY
   // geocoder: L.Control.Geocoder.google({
-  //     apiKey: 'YOUR_API_KEY' // Replace with your Google API key
+  //     apiKey: 'AIzaSyAkYLkNcozn6vdqps5Pc0-ZNfIcawiavVg' // Replace with your Google API key
   // })
+  // // Mapbox Geocoder 
+  geocoder: L.Control.Geocoder.mapbox({
+    apiKey: 'pk.eyJ1IjoiZGFlbGVuZyIsImEiOiJjbDl5d3h6NzkwOTdoM29xb20xYzJ3NmZsIn0.sMhj9jD84igqnZdX08l33A'
+  })
 }).on('markgeocode', function (event) {
   var location = event.geocode.center;
 
@@ -218,6 +358,7 @@ var geocoder = L.Control.geocoder({
   marker = L.marker(location).addTo(map);
   map.setView(location, 13);
   // Just testing getting lat lon points from leaflet-control-geocoder 
+  // This is where code is reused to test if the geosearched locations is within one of the census tracts 
   var loc = marker.getLatLng();
   console.log("lat: ", loc.lat);
   console.log("long: ", loc.lng);
@@ -242,3 +383,16 @@ var geocoder = L.Control.geocoder({
     createTextBoxControl("This address IS NOT eligible");
   }
 }).addTo(map);
+
+// Mapbox with leaflet control geocoder test 
+
+// let googleGeoCoderProvider = L.Control.Geocoder.mapbox({
+//   apiKey: 'pk.eyJ1IjoiZGFlbGVuZyIsImEiOiJjbDl5d3h6NzkwOTdoM29xb20xYzJ3NmZsIn0.sMhj9jD84igqnZdX08l33A'
+// });
+// L.Control.geocoder({
+//   placeholder: 'Search for a location...',
+//   collapsed: false,
+//   geocoder: googleGeoCoderProvider,
+// }).addTo(map);
+
+
